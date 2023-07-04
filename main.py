bec3867cc19e6f4f6d4f6e9c39f4a9cf1fd775ab
V <thohrnikk11@gmail.com>
2023-07-04 12:59:47 +0530

    init

--- main.py
+++ main.py
@@ -2,32 +2,43 @@ from telegram.ext import *
 from telegram import Update
 import keys
 import requests
+import os
+import openai
 print('Starting up bot...')
+openai.api_key=keys.openai_key
 
 
+
+def image(word):
+    image_res = openai.Image.create(
+        prompt=word,
+        n=1,
+        size="512x512"
+    )
+    print(image_res['data'][0]['url'])
+    return image_res['data'][0]['url']
 def fetch_and_display_api(word):
-    #response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Welcome to the Roast-o-Matic app, where laughter and good-natured teasing collide! Brace yourself for a hilarious experience as we whip up some delightful roasts just for you. Remember, it's all in good fun, so let's embark on a rib-tickling adventure together! "},
+            {"role": "user", "content": f"{word}"},
+            # {"role": "system", "content": "Oh, it seems you've entered the fun zone! Prepare yourself for some playful "
+            #                              "banter and a sprinkle of humor. Don't worry, it's all in good fun! Let's "
+            #                              "dive in and embrace the laughter together!"},
+            #
+            # {"role": "user", "content": f"{word}"},
+    ]
+    )
 
-    url = "https://twinword-word-graph-dictionary.p.rapidapi.com/association/"
-
-    querystring = {"entry": "mask"}
-
-    headers = {
-        "X-RapidAPI-Key": "5f96d16109msh06981ac9da7d47fp19802fjsn9c7d60796906",
-        "X-RapidAPI-Host": "twinword-word-graph-dictionary.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=querystring)
-
-    print(response.json())
-
+    print(completion.choices[0].message)
+    return completion.choices[0].message['content']
 
 # Example usage:
 
 
 
 async def start_command(update, context):
-    fetch_and_display_api()
     await update.message.reply_text('Hello there! i\'m a bot. Nice to mee you!')
 
 
@@ -41,6 +52,7 @@ async def custom_command(update, context):
 async def handle_messages(update, context):
     text = update.message.text
     definition = fetch_and_display_api(text)
+    #await update.message.reply_photo(photo=definition, caption="This is an image!")
     await update.message.reply_text(definition)
 
 
